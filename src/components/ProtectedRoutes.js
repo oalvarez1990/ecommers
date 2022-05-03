f1729b3a397ab09cd983f262b140d93a0d21f521
oalvarez1990 <omaraleyser@hotmail.com>
2022-05-02 19:59:36 -0500

    prueba

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -11,32 +11,4 @@ const ProtectedRoutes = () => {
 
 export default ProtectedRoutes;
 
-//Cruzar varoles pendiente en xxxx
 
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const AboutDescription = () => {
-//     return (
-//         <section className="about_description">
-//             <div>
-//                 <i className="fa-regular fa-hand"></i>
-//                 <h4>Satisfaction Guaranteed</h4>
-//                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-//                 <Link to="/" className="btn-shop">SHOP NOW</Link>
-//             </div>
-//             <div>
-//                 <i className="fa-solid fa-rocket"></i>
-//                 <h4>Fast Shipping</h4>
-//                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-//                 <Link to="/" className="btn-shop">SHOP NOW</Link>
-//             </div>
-//             <div>
-//                 <i className="fa-solid fa-helmet-safety"></i>
-//                 <h4>UV Protection</h4>ersds
-//                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-//                 <Link to="/" className="btn-shop">SHOP NOW</Link>
-//             </div>
-//         </section>ersssdd
-//     );
-// };
