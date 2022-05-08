43504feca5860baa937ac25d82f013d48fc53113
oalvarez1990 <omaraleyser@hotmail.com>
2022-05-07 20:42:29 -0500

    prueba

--- src/components/Home/DropDown.js
+++ src/components/Home/DropDown.js
@@ -23,3 +23,27 @@ const DropDown = ({ header, children }) => {
 export default DropDown;
 
 // 2314
+
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// const calculatorRoute = require('./routes/calculator.route');
+
+// const app = express();
+
+// app.use(bodyParser.json());
+// //  no permitira objetos anidados
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/api', calculatorRoute);
+
+// app.use((error, req, res, next) => {
+//     const status = error.statusCode || 500;
+//     const message = error.message;
+//     const data = error.data;
+//     res.status(status).json({ message: message, data: data });
+// });
+
+// module.exports = app;
