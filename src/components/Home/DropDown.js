d7ba1d5df98ecf1616f6437e30c5a24970a2379d
oalvarez1990 <omaraleyser@hotmail.com>
2022-05-08 23:10:52 -0500

    prueba

--- src/components/Home/DropDown.js
+++ src/components/Home/DropDown.js
@@ -47,3 +47,36 @@ export default DropDown;
 // });
 
 // module.exports = app;
+
+
+// ' Copia del clipboard a variable
+// Function clip2str()
+//   Dim objWord, str_x
+
+//   Set objWord = CreateObject("Word.Application")
+//   With objWord
+//     .Visible = False
+//     .Documents.Add
+//     .Selection.Paste
+//     .Selection.WholeStory
+//     str_x = .Selection.Text
+//     .Quit False
+//   End With
+//   clip2str = str_x
+// End Function
+
+
+// ' De variable a clipboard
+// Function str2clip(str_x)
+//   Dim objWord
+
+//   Set objWord = CreateObject("Word.Application")
+//   With objWord
+//     .Visible = False
+//     .Documents.Add
+//     .Selection.TypeText str_x
+//     .Selection.WholeStory
+//     .Selection.Copy
+//     .Quit False
+//   End With
+// End Function
